3545aea5c1e1f047c9ffbedd7e689d359f650ca8
LusinehTsarukyan <tsarukyanlusine@gmail.com>
2019-09-08 22:39:35 +0400

    main activity

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -11,16 +11,13 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity{
-    public static ArticlesKeeper articlesKeeper = null;
-    private final String apiKey = "6cb66347-dd59-4b6c-be55-731200528471";
+    public static ArticlesManager articlesManager = null;
+    public static final String apiKey = "6cb66347-dd59-4b6c-be55-731200528471";
     private MyRecyclerViewAdapter adapter;
     ContexManager contexManager;
 
@@ -30,27 +27,37 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         contexManager.setMainContext(this);
-        articlesKeeper = new ArticlesKeeper(this);
+        articlesManager = new ArticlesManager(this);
 
         final String urlString = "https://content.guardianapis.com/search?q=article&show-blocks=all&api-key=" + apiKey;
-        new RequestJsonTask(articlesKeeper).execute(urlString);
+        new RequestJsonTask(articlesManager).execute(urlString);
 
         // set up the RecyclerView
+//        RecyclerView horizontalRecyclerView = findViewById(R.id.pinnedArticles);
+//        LinearLayoutManager horizontalLinearLayoutManager = new LinearLayoutManager(this,
+//                LinearLayoutManager.HORIZONTAL, false);
+//        horizontalRecyclerView.setLayoutManager(horizontalLinearLayoutManager);
+//        adapter = articlesManager.adapter;
+
+
         RecyclerView recyclerView = findViewById(R.id.articles);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,
                 LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
-        adapter = articlesKeeper.adapter;
+        adapter = articlesManager.adapter;
 
         adapter.setClickListener(new MyRecyclerViewAdapter.ItemClickListener() {
             @Override
-            public void onItemClick(View view, int position) {
+            public void onItemClick(View view, int position, Article article) {
+
                 ImageView imageView =  view.findViewById(R.id.img_activity_1);
                 Intent intent = new Intent(ContexManager.getMainContext(), ArticleActivity.class);
 
                 intent.putExtra("imagedata", ((BitmapDrawable)imageView.getDrawable()).getBitmap());
-                intent.putExtra("title", ((TextView)view.findViewById(R.id.Title)).getText().toString());
-                intent.putExtra("summary", ((TextView)view.findViewById(R.id.summary)).getText().toString());
+                //intent.putExtra("title", ((TextView)view.findViewById(R.id.Title)).getText().toString());
+                //intent.putExtra("summary", ((TextView)view.findViewById(R.id.summary)).getText().toString());
+                intent.putExtra("title", article.getTitle());
+                intent.putExtra("summary", article.getSummary());
                 ActivityOptionsCompat option = ActivityOptionsCompat.makeSceneTransitionAnimation(
                         (Activity) ContexManager.getMainContext(),imageView
                        , "img_transition");
@@ -64,7 +71,7 @@ public class MainActivity extends AppCompatActivity{
 
                                       @Override
                                       public void run() {
-                                          new RequestJsonTask(articlesKeeper).execute(urlString);
+                                          new RequestJsonTask(articlesManager).execute(urlString);
                                       }
                                   },
                 0, 10000);   // 30000 Millisecond  = 30 second
